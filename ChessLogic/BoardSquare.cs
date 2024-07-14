a27c3ac53ea12750a911a56d6f4078d294329c0f
dimitar <dimiturivanov92@gmail.com>
2024-07-14 17:53:26 +0300

    chess game logic

---
+++ ChessLogic/BoardSquare.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication3.ChessLogic
+{
+    public class BoardSquare
+    {
+        public Colors Color { get; set; }   
+        public Piece? Piece { get; set; }
+    }
+}
---
+++ ChessLogic/MainClasses.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApplication3.ChessLogic
+{
+    public class AvailiableMovementDirections
+    {
+        public List<Directions> Directions { get; set; } = [];
+        public MovementTypes MovementType { get; set; }
+    }
+
+
+    public class PlayState
+    {
+        public PlayStateTypes Type { get; set; }
+        public PlayStateSubTypes SubType { get; set; } // Use specific enum type depending on Type
+
+        public Colors InitializedBy { get; set; } // Only for drawOffer type, assuming Colors is an enum
+    }
+
+
+    public class Coordinates
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public void Deconstruct(out int row, out int col)
+        {
+            row = Row;
+            col = Col;
+        }
+    }
+
+
+}
